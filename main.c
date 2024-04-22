82b7f683b60c5ce3dee9cdbe673a9fc34a69cfe5
Maria-Choix <151141825+Maria-Choix@users.noreply.github.com>
2024-04-22 10:56:52 +0200

    Create main.c

---
+++ main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "column.h"
+#include "cdataframe.h"
+
+int main() {
+    IntCol *mycol = create_col(IntCol *col, 5);
+    // utilisation fonctions gestion colonne
+    int val1 = 10, val2=11;
+
+    if (insert_value_col(mycol, val1)) {
+        printf("Value added successfully to my column\n");}
+    else {
+        printf("Error adding value to my column\n");}
+
+    if (insert_value_col(mycol, val2)) {
+        printf("Value added successfully to my column\n");}
+    else {
+        printf("Error adding value to my column\n");}
+
+    print_col(IntCol *mycol);
+
+    delete_col(IntCol **mycol);
+
+
+
+    return 0;
+}
